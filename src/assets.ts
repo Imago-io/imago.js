eaf046e0f358463a6c1628249e94350ee301320d
Sebastian Moreno <sebastiann.moreno@gmail.com>
2016-11-28 16:00:26 -0500

    test 1

--- src/assets.ts
+++ src/assets.ts
@@ -4,20 +4,25 @@ import { Utils } from './utils';
 
 export class Assets {
 
-  utils: Utils;
+  utils: any = Utils;
 
   constructor(public api: any) {}
 
   search(query: any) {
-
+    let request = {
+      path: `/assets`,
+      type: 'POST',
+      data: query
+    };
+    return this.utils.makeRequest(this.api, request);
   }
 
   get(id: string) {
     let request = {
       path: `/assets/${id}`,
       type: 'GET'
-    }
-    return this.utils.makeRequest(this.api, request)
+    };
+    return this.utils.makeRequest(this.api, request);
   }
 
   update(asset: any) {
--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,30 @@
 'use strict';
 
-import * as request from 'superagent';
+import * as http from 'superagent';
 
 export class Utils {
 
   makeRequest(api: any, request: any) {
 
-    let headers = {
-      Authorization: `Basic ${api.key}`
-    };
+    let headers: any = {};
+    if (api.key) {
+      headers.Authorization = `Basic ${api.key}`;
+    }
 
     let path = `${api.host}${api.path}${request.path}`;
+    let query = http(request.type, path);
+    console.log('query', query);
+
+    if (Object.keys(headers).length) {
+      query.set(headers);
+    }
+
+    if (request.data) {
+      query.send(request.data);
+    }
+
+    console.log('query 2', query);
+
 
   }
 
